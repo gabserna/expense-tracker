a755af48f0939d41d3b56d60f1608c923775207e
Gabriel Serna <109190797+gabserna@users.noreply.github.com>
2023-04-11 00:46:00 -0600

    changes with css file, added screenshot as example

--- src/AddTransaction.js
+++ src/AddTransaction.js
@@ -1,1 +1,50 @@
 //componente que renderizará el formulario que permitirá al usuario agregar nuevas transacciones
+import './style.css';
+
+import React, { useContext, useState } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+  // Obtener el estado y la función addTransaction del contexto global
+  const { addTransaction } = useContext(GlobalContext);
+
+  // Crear los estados locales para los campos de la transacción
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  // Manejar la acción del submit del formulario
+  const onSubmit = e => {
+    e.preventDefault();
+
+    // Crear una nueva transacción con los campos ingresados
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    };
+
+    // Añadir la nueva transacción al estado global
+    addTransaction(newTransaction);
+
+    // Reiniciar los estados locales de los campos del formulario
+    setText('');
+    setAmount(0);
+  };
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">Amount <br /> (negative - expense, positive - income)</label>
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  )
+}
---
+++ src/Balance.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Balance = ({income, expense}) => {
+    const dontKnowWhatsNextFromHere = 0;
+}
